0f8a6e80081cb9fef22f54996cfa4d6e5f36498e
gapan <gapan@2ddef7da-3bd2-46e7-9ee2-e1d7b05ddb3f>
2009-10-03 09:22:48 +0000

    compile.sh compiles files and install.sh only installs them

--- compile.sh
+++ compile.sh
@@ -5,18 +5,44 @@ name="depfinder-search"	# Name of the executable.
 bindir="bin"	# Directory where temporary binary objects are stored.
 classes="CWhatProvides"
 
-mkdir "$bindir" 2> /dev/null
+if [ "$1" == "-64" ];then
+	export CXXFLAGS=${CXXFLAGS:="-O3 -fPIC -fomit-frame-pointer"}
+else
+	export CXXFLAGS=${CXXFLAGS:="-O3 -march=i486 -mtune=i686 -fomit-frame-pointer"}
+fi
 
-for curfile in $classes; do
-	if [ "$curfile.cpp" -nt "$bindir/$curfile.o" ] || [ "$curfile.h" -nt "$bindir/$curfile.o" ]; then
-		echo "g++ -c $ccextras $CXXFLAGS \"$curfile.cpp\" -o \"$bindir/$curfile.o\""
-		g++ -c $ccextras $CXXFLAGS "$curfile.cpp" -o "$bindir/$curfile.o"
-	fi
-done
-for curfile in $classes; do
-	linkage="$linkage $bindir/$curfile.o"
-done
+if [ "$1" == "-64" ];then
+	sed -i "s/^LIBDIRSUFFIX=.*/LIBDIRSUFFIX=\"64\"/" depfinder
+else
+	sed -i "s/^LIBDIRSUFFIX=.*/LIBDIRSUFFIX=\"\"" depfinder
+fi
 
-echo "g++ $ccextras $CXXFLAGS -o \"$name\" main.cpp $linkage"
-rm -f "$name"
-g++ $ccextras $CXXFLAGS -o "$name" main.cpp $linkage
+# compile depfinder-search
+( 
+	cd depfinder-search
+
+	mkdir -p "$bindir" 2> /dev/null
+
+	for curfile in $classes; do
+		if [ "$curfile.cpp" -nt "$bindir/$curfile.o" ] || [ "$curfile.h" -nt "$bindir/$curfile.o" ]; then
+			echo "g++ -c $ccextras $CXXFLAGS \"$curfile.cpp\" -o \"$bindir/$curfile.o\""
+			g++ -c $ccextras $CXXFLAGS "$curfile.cpp" -o "$bindir/$curfile.o"
+		fi
+	done
+	for curfile in $classes; do
+		linkage="$linkage $bindir/$curfile.o"
+	done
+
+	echo "g++ $ccextras $CXXFLAGS -o \"$name\" main.cpp $linkage"
+	rm -f "$name"
+	g++ $ccextras $CXXFLAGS -o "$name" main.cpp $linkage
+)
+
+# compile locales
+(
+	cd locale
+	for i in `ls *.po`; do
+		echo "Compiling $i"
+		msgfmt $i -o `echo $i | sed "s/.po//"`.mo
+	done
+)
--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-if [ "$1" == "-64" ];then
-	export CXXFLAGS=${CXXFLAGS:="-O3 -fPIC -fomit-frame-pointer"}
-else
-	export CXXFLAGS=${CXXFLAGS:="-O3 -march=i486 -mtune=i686 -fomit-frame-pointer"}
-fi
-
-( cd depfinder-search && ./compile )
-	
 install -d -m 755 $DESTDIR/usr/bin
 install -d -m 755 $DESTDIR/usr/libexec
 install -m 755 depfinder $DESTDIR/usr/bin/
 install -m 755 depfinder-search/depfinder-search $DESTDIR/usr/libexec/
 
-if [ "$1" == "-64" ];then
-	sed -i "s/^LIBDIRSUFFIX=.*/LIBDIRSUFFIX=\"64\"/" $DESTDIR/usr/bin/depfinder
-fi
-
+for i in `ls locale/*.mo`; do
+                install -d -m 755 \
+                $DESTDIR/usr/share/locale/`basename $i|sed "s/.mo//"`/LC_MESSAGES \
+                2> /dev/null
+                install -m 644 $j \
+                $DESTDIR/usr/share/locale/`basename $i|sed "s/.mo//"`/LC_MESSAGES/depfinder.mo
+done
